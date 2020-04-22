f5c53510af066468c096153d0e736d02dd25e3ac
Olli Paasonen <spaceman@thespaceman.space>
2020-04-22 10:33:48 +0300

    Exercise 1.7 done

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+
   return (
     <div>
       <h2>statistics</h2>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
     </div>
   );
 };
